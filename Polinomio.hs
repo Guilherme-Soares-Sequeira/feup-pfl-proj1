ac8207172a3e0fdcfbbd74ebcd1db3425b0ab721
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2022-10-23 19:00:56 +0100

    Finished parser

--- Polinomio.hs
+++ Polinomio.hs
@@ -50,12 +50,12 @@ data Variable
   | NCoefAndLiteral Coef Literal
   deriving (Show)
 
---data Expr
---  = Mon    Variable     -- double constants, leaves of the expression tree
---  | Add    Expr Expr    -- addition node
---  deriving (Show)
+data Expr
+  = Mon    Variable     -- double constants, leaves of the expression tree
+  | Add    Expr Expr    -- addition node
+  | End
+  deriving (Show)
 
-type Expr = [Variable]
 
 parseChar :: [Token] -> Maybe (Char, [Token])
 parseChar (IncogTok n : restTokens)
@@ -74,41 +74,111 @@ parseCharOrExpo tokens
   = case parseChar tokens of
       Just (incog, ExpoTok : restTokens1) -> 
           case parseDouble restTokens1 of
-            Just (expo, restTokens2) -> Just (ComplexIncog incog expo, restTokens2)
-            Nothing -> Nothing
+            Just (expo, restTokens2) -> case parseCharOrExpo restTokens2 of
+              Nothing -> Just (ComplexIncog incog expo, restTokens2)
+              Just (literal, restTokens3) -> Just (Mul (ComplexIncog incog expo) literal, restTokens3)
+            result -> Nothing
       Just (incog, nextTok : restTokens1) -> case nextTok of
         IncogTok next -> case parseCharOrExpo (nextTok : restTokens1) of
           Just (literal, restTokens2) -> Just (Mul (ComplexIncog incog 1.0) literal, restTokens2)   
           result -> Nothing
-        result -> Nothing
+        result -> Just (ComplexIncog incog 1.0, nextTok : restTokens1)
       Just (incog, restTokens1) -> Just (ComplexIncog incog 1.0, restTokens1)
       result -> Nothing     -- could be 'Nothing' or a valid expression
 
 parseCoefOrIncog :: [Token] -> Maybe (Variable, [Token])
---parseCoefOrIncog [] = Just (CoefAndLiteral 0.0 Empty, [])
+parseCoefOrIncog [] = Just (CoefAndLiteral 0.0 Empty, [])
+parseCoefOrIncog (tok : rest@(atoken:arest)) =
+  case tok of
+    DoubleTok n -> case parseDouble (tok : rest) of
+      Just (coef, nextTok : restTokens1) -> case nextTok of
+        IncogTok c -> case parseCharOrExpo (nextTok : restTokens1) of
+          Just(literal, restTokens2) -> Just (CoefAndLiteral coef literal, restTokens2)
+          result -> Nothing
+        result -> Just (CoefAndLiteral coef Empty, nextTok : restTokens1)
+      result -> Nothing
+    IncogTok c -> case parseCharOrExpo (tok : rest) of
+      Just (literal, restTokens) -> Just (CoefAndLiteral 1.0 literal, restTokens)
+      result -> Nothing
+    MinusTok -> case atoken of
+      DoubleTok n -> case parseDouble (atoken : arest) of
+        Just (coef, nextTok: restTokens1) -> case nextTok of
+          IncogTok c -> case parseCharOrExpo (nextTok : restTokens1) of
+            Just(literal, restTokens2) -> Just (NCoefAndLiteral coef literal, restTokens2)
+            result -> Nothing
+          result -> Nothing
+        result -> Nothing
+      IncogTok c -> case parseCharOrExpo (atoken : arest) of
+        Just (literal, restTokens) -> Just (NCoefAndLiteral 1.0 literal, restTokens)
+        result -> Nothing
+      result -> Nothing
+    result -> Nothing
 parseCoefOrIncog (tok : rest) =
   case tok of
     DoubleTok n -> case parseDouble (tok : rest) of
       Just (coef, nextTok: restTokens1) -> case nextTok of
-        IncogTok c -> case parseCharOrExpo restTokens1 of
-          Just(literal, restTokens2) -> Just (CoefAndLiteral coef literal, restTokens1)
+        IncogTok c -> case parseCharOrExpo (nextTok : restTokens1) of
+          Just(literal, restTokens2) -> Just (CoefAndLiteral coef literal, restTokens2)
           result -> Nothing
         result -> Nothing
       result -> Nothing
     IncogTok c -> case parseCharOrExpo (tok : rest) of
       Just (literal, restTokens) -> Just (CoefAndLiteral 1.0 literal, restTokens)
       result -> Nothing
-    result -> Nothing
+
   
-parse :: [Token] -> Maybe Expr
-parse tokens
-  = case parseCoefOrIncog tokens of
-      Just (var1, PlusTok : restTokens1) -> 
-          case parseCoefOrIncog restTokens1 of
-            Just (var2, restTokens2) -> Just [var1, var2]
-            Nothing                  -> Nothing
-      Just(var1, []) -> Just [var1]
+tparse :: [Token] -> Maybe Expr
+tparse [] = Just End
+tparse [tok] =
+  case tok of
+    DoubleTok n -> case parseCoefOrIncog [tok] of
+      Just (var1, rest) -> Just (Mon var1)
       result -> Nothing
+    IncogTok c -> case parseCoefOrIncog [tok] of
+      Just (var1, rest) -> Just (Mon var1)
+      result -> Nothing
+tparse (tok : rest) =
+  case tok of 
+    PlusTok -> case parseCoefOrIncog rest of
+      Just (var, []) -> Just (Mon var)
+      Just (var, restTokens1) -> case tparse restTokens1 of
+        Just expr1 -> Just (Add (Mon var) expr1)
+        result -> Nothing
+      result -> Just End
+    DoubleTok n -> case parseCoefOrIncog (tok:rest) of
+      Just (var, []) -> Just (Mon var)
+      Just (var, restTokens1) -> case tparse restTokens1 of
+        Just expr1 -> Just (Add (Mon var) expr1)
+        result -> Nothing
+      result -> Just End
+    IncogTok c -> case parseCoefOrIncog (tok:rest) of
+      Just (var, []) -> Just (Mon var)
+      Just (var, restTokens1) -> case tparse restTokens1 of
+        Just expr1 -> Just (Add (Mon var) expr1)
+        result -> Nothing
+      result -> Nothing
+    result -> Nothing
+
+parse :: String -> Polinomio
+parse string = case tparse (lexer string) of
+  Just End -> []
+  Nothing -> error "Invalid expression"
+  Just expr -> getPol expr
+
+getLiteral :: Literal -> Char -> Double -> LiteralMap.LiteralMap Char Nat
+getLiteral Empty a b = LiteralMap.empty
+getLiteral (ComplexIncog incog expo) a b = LiteralMap.fromList [(incog, intToNat (floor expo))] 
+getLiteral (Mul x y) a b = LiteralMap.insertMultiple (LiteralMap.toList (getLiteral x ' ' 0)) (getLiteral y ' ' 0)
+
+getMon :: Variable -> Monomio.Monomio
+getMon (CoefAndLiteral coef literal) = (coef, getLiteral literal ' ' 0)
+getMon (NCoefAndLiteral coef literal) = (-coef, getLiteral literal ' ' 0)
+
+
+getPol :: Expr -> Polinomio
+getPol (Mon var) = [getMon var]
+getPol End = []
+getPol (Add expr1 expr2) = getPol expr1 ++ getPol expr2
 
 sum :: Polinomio -> Monomio.Monomio -- Assumes all elements have the same literal
 sum [x] = x
